c019e1c029614dde7178c124e89881ac357f8ff8
thanks299 <thanksagbeble@gmail.com>
2024-03-13 14:48:26 -0700

    sql

---
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -0,0 +1,4 @@
+-- script that creates the table force_name on your MySQL server
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+USE hbtn_0d_2;
+CREATE TABLE IF NOT EXISTS force_name (id INT,name VARCHAR(256) NOT NULL);

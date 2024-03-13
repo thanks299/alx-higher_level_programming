48c337230eb5b31ec2fee417df2169a02fe420df
thanks299 <139892876+thanks299@users.noreply.github.com>
2024-03-13 22:52:30 +0100

    sql

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,4 +1,5 @@
 -- script that creates the table force_name on your MySQL server
+-- if database hbtn_0d_2 already exists, your script should not fail
+CREATE TABLE IF NOT EXISTS force_name (id INT,name VARCHAR(256) NOT NULL);
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 USE hbtn_0d_2;
-CREATE TABLE IF NOT EXISTS force_name (id INT,name VARCHAR(256) NOT NULL);

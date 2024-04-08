c3189141b8337aaa8f14008153fdacbca2aa3857
root <thanksagbeble@gmail.com>
2024-04-08 14:44:25 -0700

    javascriptfile

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,14 +1,10 @@
 #!/usr/bin/node
 
-function add(a, b) {
+// Define func 'add' that takes two param. 'a' and 'b' and returns the sum
+function add (a, b) {
   return a + b;
 }
 
-const arg1 = parseInt(process.argv[2]);
-const arg2 = parseInt(process.argv[3]);
-
-if (isNaN(arg1) || isNaN(arg2)) {
-  console.log("Arguments are not integers");
-} else {
-  console.log(add(arg1, arg2));
-}
+console.log(
+  add(Number.parseInt(process.argv[2]), Number.parseInt(process.argv[3]))
+);

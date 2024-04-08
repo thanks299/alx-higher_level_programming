9a58da01af46e8d0ee15330317a56a5de81c2937
root <thanksagbeble@gmail.com>
2024-04-08 14:37:34 -0700

    javaascriptfile

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -4,7 +4,7 @@ const arg = process.argv[2];
 const size = parseInt(arg);
 
 if (isNaN(size)) {
-  console.log("Missing size");
+  console.log('Missing size');
 } else {
   for (let i = 0; i < size; i++) {
     console.log('X'.repeat(size));

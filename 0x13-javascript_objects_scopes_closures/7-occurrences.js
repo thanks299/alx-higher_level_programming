2c59a6a06341672f217061c7e3b959f6173ab541
root <thanksagbeble@gmail.com>
2024-04-09 15:19:14 -0700

    javascriptobject

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -2,7 +2,7 @@
 
 exports.nbOccurences = function (list, searchElement) {
   let count = 0;
-  
+
   // Iterate through the list
   for (let i = 0; i < list.length; i++) {
     // Check if the current element is equal to the searchElement
@@ -10,6 +10,6 @@ exports.nbOccurences = function (list, searchElement) {
       count++;
     }
   }
-  
+
   return count;
 };

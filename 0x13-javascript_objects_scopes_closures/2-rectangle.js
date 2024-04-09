26e0e4e8b11cba9a7d078b6d3c1bf8ac4a4e8ea7
root <thanksagbeble@gmail.com>
2024-04-09 15:06:51 -0700

    javascriptobject

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -2,10 +2,9 @@
 
 class Rectangle {
   constructor (w, h) {
-    if (w <= 0 || h <= 0 || isNaN(w) || isNaN(h)) {
-      // If w or h is equal to 0 or not a positive integer, create an empty object
-      return {};
-    } else {
+    // Check if both width and height are greater than 0
+    if (w > 0 && h > 0) {
+      // If true, initialize the width and height attributes
       this.width = w;
       this.height = h;
     }

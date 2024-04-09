0dd4822af0552c53be177274b71f18243a3874a2
root <thanksagbeble@gmail.com>
2024-04-09 14:55:03 -0700

    javascript object

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if (w <= 0 || h <= 0 || isNaN(w) || isNaN(h)) {
+      // If w or h is equal to 0 or not a positive integer, create an empty object
+      return {};
+    } else {
+      this.width = w;
+      this.height = h;
+    }
+  }
+}
+
+module.exports = Rectangle;

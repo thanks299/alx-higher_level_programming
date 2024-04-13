2526843bc042b81e2afa3077fcd1268bb5a55c22
thanks299 <thanksagbeble@gmail.com>
2024-04-13 09:48:02 -0700

    object mapping

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,11 +1,17 @@
 #!/usr/bin/python3
-"""Script that lists all states from the database hbtn_0e_0_usa"""
+"""A Script that lists all states from the database hbtn_0e_0_usa"""
+
 import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    # Connect to MySQL server
-    conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
+    # Connect to MySQL
+    conn = MySQLdb.connect(host="localhost",
+                            port=3306,
+                            user=sys.argv[1],
+                            passwd=sys.argv[2],
+                            db=sys.argv[3],
+                            charset="utf8")
     # Create a cursor object using cursor() method
     cur = conn.cursor()
     

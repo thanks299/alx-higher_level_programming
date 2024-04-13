07e11d97dc3af785c2ba3a6c3c6fb2d742c7ea6f
thanks299 <thanksagbeble@gmail.com>
2024-04-13 09:26:00 -0700

    object mapping

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Script that lists all states from the database hbtn_0e_0_usa"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    # Connect to MySQL server
+    conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
+    # Create a cursor object using cursor() method
+    cur = conn.cursor()
+    
+    # Execute SQL query to select all states from states table
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    
+    # Fetch all the rows in a list of lists
+    query_rows = cur.fetchall()
+    
+    # Print each row
+    for row in query_rows:
+        print(row)
+    
+    # Close cursor and connection
+    cur.close()
+    conn.close()
---
+++ 0x0F-python-object_relational_mapping/README.md
@@ -0,0 +1,1 @@
+this is my 0x0F. Python - Object-relational mapping project

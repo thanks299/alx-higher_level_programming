188bf17fa072ca2feaafbbdde80426ce7554e92d
thanks299 <thanksagbeble@gmail.com>
2024-04-15 11:17:08 -0700

    object mapping

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -15,7 +15,8 @@ if __name__ == "__main__":
             f'mysql+mysqldb://{username}:{password}@localhost:3306/{db_name}')
     Session = sessionmaker(bind=engine)
     session = Session()
-    states_with_a = session.query(State).filter(State.name.like('%a%')).order_by(State.id).all()
+    states_with_a = session.query(State).filter(
+            State.name.like('%a%')).order_by(State.id).all()
     for state in states_with_a:
         print(f"{state.id}: {state.name}")
     session.close()

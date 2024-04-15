c074c1357cb698c1d513021d954c0ad2ec4a772b
thanks299 <thanksagbeble@gmail.com>
2024-04-15 11:14:29 -0700

    object mapping

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -11,7 +11,8 @@ from model_state import Base, State
 
 if __name__ == "__main__":
     username, password, db_name = sys.argv[1], sys.argv[2], sys.argv[3]
-    engine = create_engine(f'mysql+mysqldb://{username}:{password}@localhost:3306/{db_name}')
+    engine = create_engine(
+            f'mysql+mysqldb://{username}:{password}@localhost:3306/{db_name}')
     Session = sessionmaker(bind=engine)
     session = Session()
     states_with_a = session.query(State).filter(State.name.like('%a%')).order_by(State.id).all()

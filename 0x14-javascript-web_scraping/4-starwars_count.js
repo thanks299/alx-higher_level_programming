ba124a3e4c0fc6668804d95900701c84711e5d4c
thanks299 <thanksagbeble@gmail.com>
2024-05-21 13:46:31 -0700

    web scrapping

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,35 +1,35 @@
 #!/usr/bin/node
 const request = require('request');
 
-const apiURL = 'https://swapi-api.alx-tools.com/api/films/'
+const apiURL = 'https://swapi-api.alx-tools.com/api/films/';
 
 request.get(apiURL, async (error, response, body) => {
-    if (error) {
-        console.error(error);
-    } else {
-        const movies = JSON.parse(body);
-        let wedgeMovies = 0;
-        for (const movie of movies.results) {
-            for (const characterURL of movie.characters) {
-                try {
-                    const characterResponse = await new Promise((resolve, reject) => {
-                        request.get(characterURL, (error, response, body) => {
-                            if (error) {
-                                reject(error);
-                            } else {
-                                resolve(body);
-                            }
-                        });
-                    });
-                    const character = JSON.parse(characterResponse);
-                    if (character.name === 'Wedge Antilles') {
-                        wedgeMovies++;
-                    }
-                } catch (error) {
-                    console.error(error);
-                }
-            }
+  if (error) {
+    console.error(error);
+  } else {
+    const movies = JSON.parse(body);
+    let wedgeMovies = 0;
+    for (const movie of movies.results) {
+      for (const characterURL of movie.characters) {
+        try {
+          const characterResponse = await new Promise((resolve, reject) => {
+            request.get(characterURL, (error, response, body) => {
+              if (error) {
+                reject(error);
+              } else {
+                resolve(body);
+              }
+            });
+          });
+          const character = JSON.parse(characterResponse);
+          if (character.name === 'Wedge Antilles') {
+            wedgeMovies++;
+          }
+        } catch (error) {
+          console.error(error);
         }
-        console.log(wedgeMovies);
+      }
     }
+    console.log(wedgeMovies);
+  }
 });
